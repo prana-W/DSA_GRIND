016342d09d13147efa2cb639f1cda856e3d3b844
Pranaw-NIT-JSR <pramadhu.kumar@gmail.com>
2024-12-29 20:29:56 +0530

    lec-29 revision done

--- 29_dec.cpp
+++ 29_dec.cpp
@@ -1,10 +1,66 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
 
-    cout << "Hi" << endl;
+//creating a 1-D array dynamically
+void create_arr (int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        cin >> *(arr+i);
+    }
+}
+
+//printing
+void print_arr (int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << *(arr+i) << " ";
+    }
+}
+ 
+int main() {
+//     int n;
+//     cin >> n;
+
+// // 1-D array using dynamic memory allocation
+// int *arr = new int[n];
+
+//     create_arr (arr, n);
+//     print_arr (arr, n);
+
+//     delete []arr;
+
+//     print_arr (arr, n);
+
+
+    int row, col;
+    cin >> row >> col;
+
+//2-D array using dynamic memory allocation
+int **arr2D = new int* [row];
+for (int i = 0; i < row; i++) {
+    *(arr2D+i) = new int[col];
+}
+
+//creating a 2-d array
+for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+        cin >> *(*(arr2D+i) + j);
+    }
+}
+
+//printing
+for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+        cout << arr2D[i][j] << " ";
+    }
+    cout << endl;
+}
+
+for (int i = 0; i < row; i++) {
+    delete [] arr2D[i];
+}
+
+delete [] arr2D;
+
     
+
 }
